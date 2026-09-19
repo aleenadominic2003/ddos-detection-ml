22db3f3f5326717cacaef25b959f63ea014371e4
aleenadominic2003 <aleenadominic16@gmail.com>
2026-09-19 13:48:50 +0530

    Add demo web application

---
+++ demo_app/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/products")
+def products():
+    return render_template("products.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(port=5001, debug=True)
